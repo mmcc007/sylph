a56cd0a92798bb29c1a1880ef8f936a8cb6642e3
Maurice McCabe <mmcc007@gmail.com>
2019-03-16 14:01:33 -0700

    Initial Commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:sylph/sylph.dart' as sylph;
+
+main(List<String> arguments) {
+  print('Hello world: ${sylph.calculate()}!');
+}
---
+++ test/sylph_test.dart
@@ -0,0 +1,8 @@
+import 'package:sylph/sylph.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
