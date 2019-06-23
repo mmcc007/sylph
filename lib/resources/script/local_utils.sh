986a6ef224b0445b83f952b929aed45b0d414b6e
Maurice McCabe <mmcc007@gmail.com>
2019-06-22 20:03:34 -0700

    Removed dependency on forked flutter by removing debug .ipa disabler on the fly #13

--- lib/resources/script/local_utils.sh
+++ lib/resources/script/local_utils.sh
@@ -84,6 +84,15 @@ EOF
 # currently assumes using forked version of flutter with archiving of debug .app permitted.
 # todo: remove this restriction by permitting on the fly
 build_debug_ipa() {
+    # cleanup on control-c
+    lay_interrupt_signal_traps
+
+    # checkout the xcode backend (just in case)
+    recover_disabler
+
+    # remove the debug .ipa disabler
+    remove_disabler
+
     APP_NAME="Runner"
     FINAL_APP_NAME="Debug_Runner"
     SCHEME=$APP_NAME
@@ -122,7 +131,72 @@ build_debug_ipa() {
     # rename debug .ipa to standard name
     mv "$IOS_BUILD_DIR/$APP_NAME.ipa" "$IOS_BUILD_DIR/$default_debug_ipa_name"
 
+     # checkout the xcode backend to recover disabler
+     recover_disabler
+
+     # release signal traps
+     remove_traps
+
     echo "Debug .ipa successfully created in $IOS_BUILD_DIR/$default_debug_ipa_name"
+}
+
+# temporarily remove debug .ipa archive disabler
+remove_disabler() {
+  local flutter_path="$(dirname $(dirname $(which flutter)))"
+  local xcode_backend_path="$flutter_path/packages/flutter_tools/bin/xcode_backend.sh"
+
+  local original=$(<"$xcode_backend_path")
+
+  local pattern='
+  if \[\[ \"\$ACTION\" == \"install\" \&\& \"\$build_mode\" != \"release\" \]\]\; then
+    EchoError "========================================================================"
+    EchoError "ERROR: Flutter archive builds must be run in Release mode."
+    EchoError ""
+    EchoError "To correct, ensure FLUTTER_BUILD_MODE is set to release or run:"
+    EchoError "flutter build ios --release"
+    EchoError ""
+    EchoError "then re-run Archive from Xcode."
+    EchoError "========================================================================"
+    exit -1
+  fi
+'
+  local re="(.*)$pattern(.*)"
+
+  local replaced
+
+  if [[ $original =~ $re ]]; then
+    replaced=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
+  else
+    echo "FATAL ERROR: by-passing debug archiving disabler failed"
+    exit -1
+  fi
+  echo "$replaced" > "$xcode_backend_path"
+}
+
+# recover debug .ipa archive disabler
+recover_disabler(){
+  local flutter_path="$(dirname $(dirname $(which flutter)))"
+  local xcode_backend_path="packages/flutter_tools/bin/xcode_backend.sh"
+  (cd "$flutter_path"; git checkout "$xcode_backend_path")
+  echo "Recovered disabler in xcode_backend.sh"
+}
+
+interrupt_signal_handler() {
+  echo -en "\n## Caught interrupt signal; Cleaning up and exiting \n"
+  recover_disabler
+#  exit $?
+  exit -1
+}
+
+lay_interrupt_signal_traps() {
+  # trap interrupt and terminate signals
+  trap interrupt_signal_handler SIGINT
+  trap interrupt_signal_handler SIGTERM
+}
+
+remove_traps() {
+  trap - SIGINT
+  trap - SIGTERM
 }
 
 main "$@"
