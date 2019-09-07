c5b5c432186304b37d8f6fd2ed0ecc05c59c6f6c
Maurice McCabe <mmcc007@gmail.com>
2019-09-07 14:30:40 -0700

    Moved local packages tests to own file

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,7 +1,9 @@
 import 'bundle_test.dart' as bundle_test;
 import 'concurrent_jobs_test.dart' as concurrent_jobs_test;
+import 'local_packages_test.dart' as local_packages_test;
 
 main() {
-  bundle_test.main();
+//  bundle_test.main();
   concurrent_jobs_test.main();
+  local_packages_test.main();
 }
