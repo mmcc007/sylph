fa8c2d79aacc31c259ccac04a9ae9b876eb82abd
Maurice McCabe <mmcc007@gmail.com>
2019-09-07 17:33:18 -0700

    Added device farm tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,9 +1,11 @@
 import 'bundle_test.dart' as bundle_test;
 import 'concurrent_jobs_test.dart' as concurrent_jobs_test;
+import 'device_farm_test.dart' as device_farm_test;
 import 'local_packages_test.dart' as local_packages_test;
 
 main() {
   bundle_test.main();
   concurrent_jobs_test.main();
+  device_farm_test.main();
   local_packages_test.main();
 }
---
+++ test/device_farm_test.dart
@@ -0,0 +1,65 @@
+import 'package:fake_process_manager/fake_process_manager.dart';
+import 'package:process/process.dart';
+import 'package:sylph/src/device_farm.dart';
+import 'package:test/test.dart';
+import 'package:tool_base/tool_base.dart';
+import 'package:tool_base_test/tool_base_test.dart';
+
+main() {
+  group('device farm', () {
+    FakeProcessManager fakeProcessManager;
+
+    setUp(() async {
+      fakeProcessManager = FakeProcessManager();
+    });
+
+    testUsingContext('setup project', () {
+      final projectName = 'project name';
+      final jobTimeoutMinutes = 10;
+      final projectArn =
+          'arn:aws:devicefarm:us-west-2:122621792560:project:9796b48e-ad3d-4b3c-97a6-94d4e50b1792';
+      fakeProcessManager.calls = [
+        Call('aws devicefarm list-projects',
+            ProcessResult(0, 0, '{"projects":[]}', '')),
+        Call(
+            'aws devicefarm create-project --name $projectName --default-job-timeout-minutes $jobTimeoutMinutes',
+            ProcessResult(
+                0,
+                0,
+                '{"project": {"arn": "$projectArn","name": "$projectName","defaultJobTimeoutMinutes": $jobTimeoutMinutes,"created": 1567902055.614}}',
+                '')),
+      ];
+      final result = setupProject(projectName, jobTimeoutMinutes);
+      expect(result, equals(projectArn));
+      fakeProcessManager.verifyCalls();
+    }, overrides: <Type, Generator>{
+      ProcessManager: () => fakeProcessManager,
+//      Logger: () => VerboseLogger(StdoutLogger()),
+    });
+
+    testUsingContext('setup device pool', () {
+      final projectName = 'project name';
+      final jobTimeoutMinutes = 10;
+      final projectArn =
+          'arn:aws:devicefarm:us-west-2:122621792560:project:9796b48e-ad3d-4b3c-97a6-94d4e50b1792';
+      fakeProcessManager.calls = [
+        Call('aws devicefarm list-projects',
+            ProcessResult(0, 0, '{"projects":[]}', '')),
+        Call(
+            'aws devicefarm create-project --name $projectName --default-job-timeout-minutes $jobTimeoutMinutes',
+            ProcessResult(
+                0,
+                0,
+                '{"project": {"arn": "$projectArn","name": "$projectName","defaultJobTimeoutMinutes": $jobTimeoutMinutes,"created": 1567902055.614}}',
+                '')),
+      ];
+      final devicePoolInfo = {};
+      final result = setupDevicePool(devicePoolInfo, projectArn);
+      expect(result, equals(projectArn));
+      fakeProcessManager.verifyCalls();
+    }, overrides: <Type, Generator>{
+      ProcessManager: () => fakeProcessManager,
+//      Logger: () => VerboseLogger(StdoutLogger()),
+    });
+  });
+}
