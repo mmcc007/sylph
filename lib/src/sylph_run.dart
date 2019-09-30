39de184beabcc16aa20aee93d4b0e565329d539b
Đào Hoàng Sơn <daohoangson@gmail.com>
2019-09-30 13:42:03 +0700

    Fix bug suite reruns because of jobArgs
    Reproduce: https://github.com/daohoangson/flutter_widget_from_html/pull/63/
    Run log: https://cirrus-ci.com/task/6239143664812032

--- lib/src/sylph_run.dart
+++ lib/src/sylph_run.dart
@@ -58,12 +58,12 @@ Future<bool> sylphRun(String configFilePath, String sylphRunName,
   // Bundle tests
   bundleFlutterTests(config);
 
-  // gather job args
-  final jobArgs = <Map>[];
-
   bool isConcurrentRun() => config.concurrentRuns ?? false;
   for (var testSuite in config.testSuites) {
     printStatus('\nRunning \'${testSuite.name}\' test suite...\n');
+
+    // gather job args
+    final jobArgs = <Map>[];
 
     // Initialize device pools and run tests in each pool
     for (final poolName in testSuite.poolNames) {
