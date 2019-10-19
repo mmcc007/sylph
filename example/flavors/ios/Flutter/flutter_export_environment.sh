cad7e297e1b5e4b947f8ab5bafe24f6440a71259
Maurice McCabe <mmcc007@gmail.com>
2019-10-18 20:09:54 -0700

    Changed name of schemes in ios to match name of flavors in android

--- example/flavors/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jenkins/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jenkins/dev/github.com/mmcc007/sylph/example/flavors"
-export "FLUTTER_TARGET=test_driver/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/jenkins/Library/flutter/bin/cache/artifacts/engine/ios"
