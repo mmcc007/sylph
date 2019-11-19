69a249f499b9d00c7884fd25178e42710f5ec41b
Maurice McCabe <mmcc007@gmail.com>
2019-11-18 22:30:20 -0800

    Removed env dependencies to permit running in CI

--- test/base/crash_reporting_test.dart
+++ test/base/crash_reporting_test.dart
@@ -11,28 +11,15 @@ import 'dart:convert';
 import 'package:file/file.dart';
 import 'package:file/local.dart';
 import 'package:file/memory.dart';
-//import 'package:flutter_tools/runner.dart' as tools;
-//import 'package:flutter_tools/src/base/context.dart';
-//import 'package:flutter_tools/src/base/io.dart';
-//import 'package:flutter_tools/src/base/logger.dart';
-//import 'package:flutter_tools/src/base/platform.dart';
-//import 'package:flutter_tools/src/cache.dart';
-//import 'package:flutter_tools/src/reporting/reporting.dart';
-//import 'package:flutter_tools/src/runner/flutter_command.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
-import 'package:pedantic/pedantic.dart';
 import 'package:reporting/reporting.dart';
 import 'package:sylph/runner.dart' as tools;
-//import 'package:sylph/runner.dart';
 import 'package:sylph/src/base/runner/sylph_command.dart';
 import 'package:test/test.dart';
+import 'package:tool_base/src/base/common.dart' as c;
 import 'package:tool_base/tool_base.dart';
 import 'package:tool_base_test/tool_base_test.dart';
-import 'package:tool_base/src/base/common.dart' as c;
-import 'package:pedantic/pedantic.dart' hide unawaited;
-//import '../src/common.dart';
-//import '../src/context.dart';
 
 void main() {
   group('crash reporting', () {
@@ -42,7 +29,7 @@ void main() {
 
     setUp(() async {
       tools.crashFileSystem = MemoryFileSystem();
-      setExitFunctionForTests((_) { });
+      setExitFunctionForTests((_) {});
     });
 
     tearDown(() {
@@ -53,7 +40,11 @@ void main() {
     testUsingContext('should send crash reports', () async {
       final RequestInfo requestInfo = RequestInfo();
 
-      tools.crashReportSender=CrashReportSender(MockCrashReportSender(requestInfo), tools.kCrashServerHost, tools.kCrashEndpointPath, tools.kProductId);
+      tools.crashReportSender = CrashReportSender(
+          MockCrashReportSender(requestInfo),
+          tools.kCrashServerHost,
+          tools.kCrashEndpointPath,
+          tools.kProductId);
       final int exitCode = await tools.run(
         <String>['crash'],
         <SylphCommand>[_CrashCommand()],
@@ -65,6 +56,8 @@ void main() {
       await verifyCrashReportSent(requestInfo);
     }, overrides: <Type, Generator>{
       Stdio: () => const _NoStderr(),
+      Platform: () =>
+          FakePlatform.fromPlatform(const LocalPlatform())..environment = {},
     });
 
     testUsingContext('should send crash reports when async throws', () async {
@@ -75,7 +68,11 @@ void main() {
 
       final RequestInfo requestInfo = RequestInfo();
 
-      tools.crashReportSender=CrashReportSender(MockCrashReportSender(requestInfo), tools.kCrashServerHost, tools.kCrashEndpointPath, tools.kProductId);
+      tools.crashReportSender = CrashReportSender(
+          MockCrashReportSender(requestInfo),
+          tools.kCrashServerHost,
+          tools.kCrashEndpointPath,
+          tools.kProductId);
 
       c.unawaited(tools.run(
         <String>['crash'],
@@ -87,13 +84,17 @@ void main() {
       await verifyCrashReportSent(requestInfo);
     }, overrides: <Type, Generator>{
       Stdio: () => const _NoStderr(),
+      Platform: () =>
+          FakePlatform.fromPlatform(const LocalPlatform())..environment = {},
     });
 
-    testUsingContext('should not send a crash report if on a user-branch', () async {
+    testUsingContext('should not send a crash report if on a user-branch',
+        () async {
       String method;
       Uri uri;
 
-      tools.crashReportSender=CrashReportSender(MockClient((Request request) async {
+      tools.crashReportSender =
+          CrashReportSender(MockClient((Request request) async {
         method = request.method;
         uri = request.url;
 
@@ -124,7 +125,8 @@ void main() {
 
     testUsingContext('can override base URL', () async {
       Uri uri;
-      tools.crashReportSender=CrashReportSender(MockClient((Request request) async {
+      tools.crashReportSender =
+          CrashReportSender(MockClient((Request request) async {
         uri = request.url;
         return Response('test-report-id', 200);
       }), tools.kCrashServerHost, tools.kCrashEndpointPath, tools.kProductId);
@@ -140,25 +142,28 @@ void main() {
 
       // Verify that we sent the crash report.
       expect(uri, isNotNull);
-      expect(uri, Uri(
-        scheme: 'https',
-        host: 'localhost',
-        port: 12345,
-        path: '/fake_server',
-        queryParameters: <String, String>{
-          'product': tools.kProductId,
-          'version': 'test-version',
-        },
-      ));
+      expect(
+          uri,
+          Uri(
+            scheme: 'https',
+            host: 'localhost',
+            port: 12345,
+            path: '/fake_server',
+            queryParameters: <String, String>{
+              'product': tools.kProductId,
+              'version': 'test-version',
+            },
+          ));
     }, overrides: <Type, Generator>{
       Platform: () => FakePlatform(
-        operatingSystem: 'linux',
-        environment: <String, String>{
-          'HOME': '/',
-          'SYLPH_CRASH_SERVER_BASE_URL': 'https://localhost:12345/fake_server',
-        },
-        script: Uri(scheme: 'data'),
-      ),
+            operatingSystem: 'linux',
+            environment: <String, String>{
+              'HOME': '/',
+              'SYLPH_CRASH_SERVER_BASE_URL':
+                  'https://localhost:12345/fake_server',
+            },
+            script: Uri(scheme: 'data'),
+          ),
       Stdio: () => const _NoStderr(),
     });
   });
@@ -173,18 +178,20 @@ class RequestInfo {
 Future<void> verifyCrashReportSent(RequestInfo crashInfo) async {
   // Verify that we sent the crash report.
   expect(crashInfo.method, 'POST');
-  expect(crashInfo.uri, Uri(
+  expect(
+      crashInfo.uri,
+      Uri(
 //    scheme: 'https',
-    scheme: 'http',
-    host: tools.kCrashServerHost,
+        scheme: 'http',
+        host: tools.kCrashServerHost,
 //    port: 443,
-    port: 8080,
-    path: tools.kCrashEndpointPath,
-    queryParameters: <String, String>{
-      'product': tools.kProductId,
-      'version': 'test-version',
-    },
-  ));
+        port: 8080,
+        path: tools.kCrashEndpointPath,
+        queryParameters: <String, String>{
+          'product': tools.kProductId,
+          'version': 'test-version',
+        },
+      ));
 //  expect(crashInfo.fields['uuid'], '00000000-0000-4000-0000-000000000000');
   expect(crashInfo.fields['product'], tools.kProductId);
   expect(crashInfo.fields['version'], 'test-version');
@@ -196,57 +203,61 @@ Future<void> verifyCrashReportSent(RequestInfo crashInfo) async {
   expect(crashInfo.fields['comments'], 'crash');
 
   final BufferLogger logger = context.get<Logger>();
-  expect(logger.statusText, 'Sending crash report to Sylph.\n'
+  expect(
+      logger.statusText,
+      'Sending crash report to Sylph.\n'
       'Crash report sent (report ID: test-report-id)\n');
 
   // Verify that we've written the crash report to disk.
-  final List<String> writtenFiles =
-  (await tools.crashFileSystem.directory('/').list(recursive: true).toList())
-      .map((FileSystemEntity e) => e.path).toList();
+  final List<String> writtenFiles = (await tools.crashFileSystem
+          .directory('/')
+          .list(recursive: true)
+          .toList())
+      .map((FileSystemEntity e) => e.path)
+      .toList();
   expect(writtenFiles, hasLength(1));
   expect(writtenFiles, contains('sylph_01.log'));
 }
 
 class MockCrashReportSender extends MockClient {
-  MockCrashReportSender(RequestInfo crashInfo) : super((Request request) async {
-      crashInfo.method = request.method;
-      crashInfo.uri = request.url;
+  MockCrashReportSender(RequestInfo crashInfo)
+      : super((Request request) async {
+          crashInfo.method = request.method;
+          crashInfo.uri = request.url;
 
-      // A very ad-hoc multipart request parser. Good enough for this test.
-      String boundary = request.headers['Content-Type'];
-      boundary = boundary.substring(boundary.indexOf('boundary=') + 9);
-      crashInfo.fields = Map<String, String>.fromIterable(
-        utf8.decode(request.bodyBytes)
-            .split('--$boundary')
-            .map<List<String>>((String part) {
-          final Match nameMatch = RegExp(r'name="(.*)"').firstMatch(part);
-          if (nameMatch == null)
-            return null;
-          final String name = nameMatch[1];
-          final String value = part.split('\n').skip(2).join('\n').trim();
-          return <String>[name, value];
-        })
-            .where((List<String> pair) => pair != null),
-        key: (dynamic key) {
-          final List<String> pair = key;
-          return pair[0];
-        },
-        value: (dynamic value) {
-          final List<String> pair = value;
-          return pair[1];
-        },
-      );
+          // A very ad-hoc multipart request parser. Good enough for this test.
+          String boundary = request.headers['Content-Type'];
+          boundary = boundary.substring(boundary.indexOf('boundary=') + 9);
+          crashInfo.fields = Map<String, String>.fromIterable(
+            utf8
+                .decode(request.bodyBytes)
+                .split('--$boundary')
+                .map<List<String>>((String part) {
+              final Match nameMatch = RegExp(r'name="(.*)"').firstMatch(part);
+              if (nameMatch == null) return null;
+              final String name = nameMatch[1];
+              final String value = part.split('\n').skip(2).join('\n').trim();
+              return <String>[name, value];
+            }).where((List<String> pair) => pair != null),
+            key: (dynamic key) {
+              final List<String> pair = key;
+              return pair[0];
+            },
+            value: (dynamic value) {
+              final List<String> pair = value;
+              return pair[1];
+            },
+          );
 
-      return Response(
-        'test-report-id',
-        200,
-      );
-    });
+          return Response(
+            'test-report-id',
+            200,
+          );
+        });
 }
 
 /// Throws a random error to simulate a CLI crash.
 class _CrashCommand extends SylphCommand {
-
   @override
   String get description => 'Simulates a crash';
 
@@ -275,7 +286,6 @@ class _CrashCommand extends SylphCommand {
 
 /// Throws StateError from async callback.
 class _CrashAsyncCommand extends SylphCommand {
-
   @override
   String get description => 'Simulates a crash';
 
@@ -308,32 +318,32 @@ class _NoopIOSink implements IOSink {
   set encoding(_) => throw UnsupportedError('');
 
   @override
-  void add(_) { }
+  void add(_) {}
 
   @override
-  void write(_) { }
+  void write(_) {}
 
   @override
-  void writeAll(_, [ __ = '' ]) { }
+  void writeAll(_, [__ = '']) {}
 
   @override
-  void writeln([ _ = '' ]) { }
+  void writeln([_ = '']) {}
 
   @override
-  void writeCharCode(_) { }
+  void writeCharCode(_) {}
 
   @override
-  void addError(_, [ __ ]) { }
+  void addError(_, [__]) {}
 
   @override
-  Future<dynamic> addStream(_) async { }
+  Future<dynamic> addStream(_) async {}
 
   @override
-  Future<dynamic> flush() async { }
+  Future<dynamic> flush() async {}
 
   @override
-  Future<dynamic> close() async { }
+  Future<dynamic> close() async {}
 
   @override
-  Future<dynamic> get done async { }
+  Future<dynamic> get done async {}
 }
